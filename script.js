7f0b650d5a4a66b751d841833dec14ba6bb7158a
Kim Leung <71901468+devworkerkim@users.noreply.github.com>
2021-09-02 11:36:22 -0600

    DOM manipulation practice changes
    feat: add UI
    refactor: change game logic to play until one scores 5 points

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const playerRock = document.querySelector('#rock');
+const playerPaper = document.querySelector('#paper');
+const playerScissors = document.querySelector('#scissors');
+const result = document.querySelector('#result');
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
+var player = 0;
+var computer = 0;
+
 function computerPlay() {
     let play = ['rock', 'paper', 'scissors'];
     return play[Math.floor(Math.random() * 3)]
@@ -7,28 +16,49 @@ function playRound(playerSelection, computerSelection = computerPlay()) {
     playerSelection = playerSelection.toLowerCase();
     switch (playerSelection) {
         case 'rock':
-            if (computerSelection === 'rock') return 'Draw';
-            else if (computerSelection === 'scissors') return 'You Win! Rock beats Scissors';
-            else if (computerSelection === 'paper') return 'You Lose! Paper beats Rock';
+            if (computerSelection === 'rock') result.innerText = 'Draw';
+            else if (computerSelection === 'scissors') {
+                result.innerText = 'Player Point! Rock beats Scissors';
+                updateScore(true);
+            } else if (computerSelection === 'paper') {
+                result.innerText = 'Computer Point! Paper beats Rock';
+                updateScore(false);
+            }
             break;
         case 'paper':
-            if (computerSelection === 'rock') return 'You Win! Paper beats Rock';
-            else if (computerSelection === 'scissors') return 'You Lose! Scissors beats Paper';
-            else if (computerSelection === 'paper') return 'Draw';
+            if (computerSelection === 'rock') {
+                result.innerText = 'Player Point! Paper beats Rock';
+                updateScore(true);
+            } else if (computerSelection === 'scissors') {
+                result.innerText = 'Computer Point! Scissors beats Paper';
+                updateScore(false);
+            } else if (computerSelection === 'paper') result.innerText = 'Draw';
             break;
         case 'scissors':
-            if (computerSelection === 'rock') return 'You Lose! Rock beats Scissors';
-            else if (computerSelection === 'scissors') return 'Draw';
-            else if (computerSelection === 'paper') return 'You Win! Scissors beats Paper';
+            if (computerSelection === 'rock') {
+                result.innerText = 'Computer Point! Rock beats Scissors';
+                updateScore(false);
+            } else if (computerSelection === 'scissors') result.innerText = 'Draw';
+            else if (computerSelection === 'paper') {
+                result.innerText = 'Player Point! Scissors beats Paper';
+                updateScore(true);
+            }
             break;
         default:
             return 'Invalid player selection.  Try again.'
     }
 }
 
-function fiveGames() {
-    for (let i = 1; i <= 5; i++) {
-        let playerSelection = prompt('Rock, Paper, or Scissors?');
-        console.log(`Game ${i}: ${playRound(playerSelection)}`);
-    }
+function updateScore(win) {
+    if (player === 5 || computer === 5) player = 0, computer = 0;
+    if (win) player++;
+    else if (!win) computer++;
+    playerScore.innerText = player;
+    computerScore.innerText = computer;
+    if (player === 5) playerScore.innerText += ' Winner!';
+    if (computer === 5) computerScore.innerText += ' Winner!';
 }
+
+playerRock.addEventListener('click', () => playRound('rock'));
+playerPaper.addEventListener('click', () => playRound('paper'));
+playerScissors.addEventListener('click', () => playRound('scissors'));
